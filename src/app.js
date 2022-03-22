40e84d74ad8d00c73a58589798b5ff111386c1a6
Tony ID <tony.inuma@gmail.com>
2022-03-22 15:51:58 -0500

    Feat: views configuration

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
 app.set("port", process.env.PORT || 3000);
+
+// Views
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 app.listen(app.get("port"), () => {
     console.log(`Server started on port ${app.get("port")}`);
 });
