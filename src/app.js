3e9e816c3c3789a138905d72269fb356cb8efa05
Tony ID <tony.inuma@gmail.com>
2022-03-22 16:39:24 -0500

    Add: database connection mysql

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const path = require("path");
 const morgan = require("morgan");
+const mysql = require("mysql");
+const myConnection = require("express-myconnection");
 const app = express();
 
 app.set("port", process.env.PORT || 3000);
@@ -10,7 +12,20 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 // Middlewares
-app.use(morgan('dev'));
+app.use(morgan("dev"));
+app.use(
+    myConnection(
+        mysql,
+        {
+            host: "localhost",
+            user: "root",
+            password: "",
+            port: "3306",
+            database: "appform_db",
+        },
+        "single"
+    )
+);
 
 app.listen(app.get("port"), () => {
     console.log(`Server started on port ${app.get("port")}`);
